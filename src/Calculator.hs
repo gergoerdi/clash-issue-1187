362a67e4b2b622a152691fcc2166dd5b953e2c47
Dr. ERDI Gergo <gergo@erdi.hu>
2020-03-09 22:28:56 +0800

    simplify

--- src/Calculator.hs
+++ src/Calculator.hs
@@ -64,27 +64,19 @@ serialDisplay
     => Signal dom Bool
     -> Signal dom (Vec n (Maybe Digit))
     -> Signal dom (Maybe (Unsigned 8))
-serialDisplay ack digits = mealyStateB step (Nothing @(Index (7 + n)), repeat 0) (ack, digits)
+serialDisplay ack digits = mealyStateB step (Nothing @(Index n), repeat 0) (ack, digits)
   where
+    step :: (Bool, Vec n (Maybe Digit)) -> State (Maybe (Index n), Vec n (Unsigned 8)) (Maybe (Unsigned 8))
     step (next, digits) = do
         (i, bs) <- get
         case i of
             Nothing -> do
-                let bs' = clear ++ map fromDigit digits
+                let bs' = map fromDigit digits
                 when (bs /= bs') $ put (Just 0, bs')
                 return Nothing
-            Just i -> do
-                when next $ put (succIdx i, rotateLeftS bs (SNat @1))
-                return . Just $ head bs
 
     fromDigit :: Maybe Digit -> Unsigned 8
     fromDigit = maybe (ByteChar ' ') $ \n -> ByteChar '0' + fromIntegral n
-
-    clear :: Vec 7 (Unsigned 8)
-    clear =
-        0x1b :> ByteChar '[' :> ByteChar '2' :> ByteChar 'J' :> -- clear screen
-        0x1b :> ByteChar '[' :> ByteChar 'H' :>                 -- cursor to home position
-        Nil
 
 logic
     :: forall n dom. (KnownNat n, HiddenClockResetEnable dom)
